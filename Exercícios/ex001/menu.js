53bcdcfb935826d07966f9d17d4680635c240d52
Artur G. Schneider <agsartur@hotmail.com>
2024-02-26 10:28:11 -0300

    básico

--- Exercícios/ex001/menu.js
+++ Exercícios/ex001/menu.js
@@ -1,3 +1,17 @@
-window.alert('Amo minha mulher')
-window.confirm('Ela é a pessoa mais incrível do mundo, tá?!')
-window.prompt('Digite algo legal para ela:')
+window.alert('Eu posso deixar alertas')
+window.confirm('Também posso pedir a confirmação de algo')
+var nome = window.prompt('Digite seu nome:')
+window.alert('Olá ' + nome + ', tudo bem?')
+
+document.write(`<p>Olá, ${nome}! Seu nome tem ${nome.length} letras.</p>`)
+document.write(`<p>Seu nome em maiúsculas é: ${nome.toUpperCase()}</p>`)
+document.write(`<p>Seu nome em minúsculas é: ${nome.toLowerCase()}</p>`)
+
+var n1 = Number.parseFloat(window.prompt('Digite um número:')) //string
+var n2 = Number(window.prompt('Digite outro número:'))
+var s = n1 + n2
+window.alert(`A soma entre ${n1} e ${n2} é: ${s}`)
+
+var n3 = Number(window.prompt(`Qual é o seu salário?`))
+window.alert(`Então seu salário é de R$ ${n3}`)
+// Fazer comentários é importantíssimo
