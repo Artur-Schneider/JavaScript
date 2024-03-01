ba0f3510fd367334de31190f170fba1ea42fe83a
Artur G. Schneider <agsartur@hotmail.com>
2024-02-29 21:34:03 -0300

    desafio

--- Exercícios/DESAFIOS/Desafio04/menu.js
+++ Exercícios/DESAFIOS/Desafio04/menu.js
@@ -1,7 +1,17 @@
 function gerar() {
     var num = document.getElementById('num')
     var n = Number(num.value)
-    if (n == ' ') {
-        window.alert('[ERRO]')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('[ERRO] Por favor, digite um número!')
+    } else {
+        let c = 1
+        tab.innerHTML = ` `
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
